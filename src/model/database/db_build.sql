626c0de8f2e90c24155b44f7bfe464e7d2b1324c
Freddie <freddiepreece@outlook.com>
2019-08-22 14:40:15 +0100

    Add constraints to database tables

--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -17,7 +17,8 @@ CREATE TABLE post (
     post_id INT PRIMARY KEY AUTO_INCREMENT, 
     post_title VARCHAR(100) NOT NULL, 
     post_content TEXT NOT NULL, 
-    user_id INT, FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE, 
+    user_id INT, 
+    CONSTRAINT post_author FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE, 
     post_date VARCHAR(10) NOT NULL
     );
 INSERT INTO post (post_title, post_content, user_id, post_date) 
@@ -26,11 +27,11 @@ VALUES ('Breaking News', 'MySQL sucks', (SELECT user_id FROM user WHERE user_nam
 CREATE TABLE comment (
     comment_id INT PRIMARY KEY AUTO_INCREMENT, 
     comment_content TEXT NOT NULL,
-    user_id INT, FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE, 
-    post_id INT, FOREIGN KEY (post_id) REFERENCES post(post_id) ON DELETE CASCADE, 
+    user_id INT, CONSTRAINT comment_author FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE, 
+    post_id INT, CONSTRAINT comment_parent FOREIGN KEY (post_id) REFERENCES post(post_id) ON DELETE CASCADE, 
     comment_date VARCHAR(10) NOT NULL
 );
 INSERT INTO comment (comment_content, user_id, post_id, comment_date) 
-VALUES ('I know right?', (SELECT user_id FROM user WHERE user_name LIKE 'foo'), '1', '2019');
+VALUES ('I know right?', (SELECT user_id FROM user WHERE user_name LIKE 'bar'), '1', '2019');
 
 COMMIT;
