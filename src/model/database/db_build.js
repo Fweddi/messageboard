98766dbecfbdc221beace974eb327249c777884c
Freddie <freddiepreece@outlook.com>
2019-08-22 20:17:21 +0100

    Refactor db_build; change comment test to adjust for new timestamps

--- src/model/database/db_build.js
+++ src/model/database/db_build.js
@@ -1,24 +1,10 @@
-const mysql = require('mysql');
-const config = require('../../../config');
-
+const connection = require('./db_connect');
 const fs = require('fs');
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
 let queries = sql.split(';');
 queries.pop();
 
-let { DB_URL, TEST_DB_URL } = config;
-
-if (process.env.NODE_ENV === 'test') {
-    DB_URL = TEST_DB_URL;
-}
-
-DB_URL = TEST_DB_URL;
-
-let connection = mysql.createConnection(DB_URL);
-
 connection.connect();
-
 queries.forEach(query => connection.query(query));
-
 connection.end();
