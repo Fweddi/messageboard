ce4d1f725e4af675e53c19f40a5d62ba3db19440
Freddie <freddiepreece@outlook.com>
2019-08-23 18:39:01 +0100

    Add cookie upon register and redirect

--- api/app.js
+++ api/app.js
@@ -5,10 +5,14 @@ const helmet = require('helmet');
 
 const escapeHtml = require('escape-html');
 const selectUserByName = require('./model/queries/select/select_user_by_name');
+const selectUserByID = require('./model/queries/select/select_user_by_ID');
 const insertUser = require('./model/queries/insert/insert_user');
 const checkUsername = require('./utils/check_username');
 const checkPassword = require('./utils/check_password');
 const bcrypt = require('bcrypt');
+const { sign, verify } = require('jsonwebtoken');
+
+const secret = process.env.SECRET;
 
 require('dotenv').config();
 
@@ -45,13 +49,27 @@ app.post('/api/form-submit', (req, res) => {
 
         const userAlreadyTaken = () => {
             // Here we would send something back to the front end to signify username has been taken.
+
+            res.writeHead(422, {
+                Location: "/"
+            });
         }
 
         const addNewUser = (username, password) => {
             bcrypt.genSalt(10)
                 .then(salt => bcrypt.hash(password, salt))
                 .then(hash => insertUser(username, hash, Date.now()))
-                .catch(err => console.error(err))
+                .then(() => selectUserByName(username))
+                .then(result => {
+                    const cookie = sign(`loggedIn=${result.user_id}`, secret);
+
+                    res.writeHead(302, {
+                        Location: "/board",
+                        "Set-Cookie": `loggedIn=${cookie}; HttpOnly; Max-Age=10000`
+                    });
+                    return res.end();
+                })
+                .catch(err => console.error(err));
         }
 
         if ((checkUsername(username) && checkPassword(password))) {
@@ -61,7 +79,8 @@ app.post('/api/form-submit', (req, res) => {
         };
     });
 
-    res.send('Hi');
+
+    // Handle redirect, add cookie?
 });
 
 app.set('PORT', process.env.PORT || 9000);
