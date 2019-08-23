2cdc3fe97d2d7824c0ade2351e0249905ad1171e
Freddie <freddiepreece@outlook.com>
2019-08-23 13:22:55 +0100

    Add client-side and server-side validation for login form

--- src/components/Register.js
+++ src/components/Register.js
@@ -18,29 +18,33 @@ class Register extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         const data = new FormData(event.target);
-        console.log(stringifyFormData(data));
+        const JSONdata = stringifyFormData(data);
 
         fetch('/api/form-submit', {
             method: 'POST',
-            body: data,
-        });
+            body: JSONdata,
+        }).catch(error => console.error(error));
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <h2>REGISTER</h2>
-                <label htmlFor="username">Username</label>
-                <input id="username" name="username" type="text" />
+            <React.Fragment>
+                <form onSubmit={this.handleSubmit}>
+                    <h2>REGISTER</h2>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" name="username" type="text" required />
 
-                <label htmlFor="password">Password</label>
-                <input id="password" name="password" type="password" />
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" pattern='/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$/' required />
 
-                <label htmlFor="password">Confirm Password</label>
-                <input id="password__confirm" name="password__confirm" type="password" />
+                    <label htmlFor="password">Confirm Password</label>
+                    <input id="password__confirm" name="password__confirm" type="password" pattern='/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$/' required />
 
-                <button>Submit</button>
-            </form >
+                    <button>Submit</button>
+                    <p>Your password must be at least eight characters long.</p>
+                    <p>It must also contain at least one uppercase letter, one lowercase letter, one number and one special character.</p>
+                </form >
+            </React.Fragment>
         );
     }
 }
