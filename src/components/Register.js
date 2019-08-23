b0a2de4be4cfc987a9df978857887fe96f29757d
Freddie <freddiepreece@outlook.com>
2019-08-23 15:14:12 +0100

    Add more validation on the client side for matching passwords

--- src/components/Register.js
+++ src/components/Register.js
@@ -13,6 +13,24 @@ class Register extends React.Component {
     constructor() {
         super();
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.state = { password: null, password__confirm: null, match: true };
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({ [name]: value });
+    }
+
+    handleBlur() {
+        if (this.state.password === this.state.password__confirm) {
+            this.setState({ ['match']: true })
+        } else {
+            this.setState({ ['match']: false })
+        }
     }
 
     handleSubmit(event) {
@@ -35,12 +53,12 @@ class Register extends React.Component {
                     <input id="username" name="username" type="text" required />
 
                     <label htmlFor="password">Password</label>
-                    <input id="password" name="password" type="password" pattern='/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$/' required />
+                    <input id="password" name="password" type="password" pattern='^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$' value={this.state.value} onChange={this.handleChange} required />
 
                     <label htmlFor="password">Confirm Password</label>
-                    <input id="password__confirm" name="password__confirm" type="password" pattern='/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$/' required />
+                    <input id="password__confirm" name="password__confirm" type="password" pattern='^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$' value={this.state.value} onChange={this.handleChange} onBlur={this.handleBlur} required />
+                    {!this.state.match ? <div class="match"> Passwords must match! </div> : <button>Submit</button>}
 
-                    <button>Submit</button>
                     <p>Your password must be at least eight characters long.</p>
                     <p>It must also contain at least one uppercase letter, one lowercase letter, one number and one special character.</p>
                 </form >
@@ -48,5 +66,6 @@ class Register extends React.Component {
         );
     }
 }
+
 
 export default Register;
